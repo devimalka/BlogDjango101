cc55fc05e522caba1892b8e23b8095754745e932
devimalka <developerimalka@gmail.com>
2021-10-06 13:49:41 +0530

    installed app and makemigrations and migrated

--- sinhaladev/settings.py
+++ sinhaladev/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogdev.apps.BlogdevConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
