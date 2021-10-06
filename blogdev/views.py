58a0e1df8fa17079b42af49683b059cb10cf87bd
devimalka <developerimalka@gmail.com>
2021-10-06 16:50:35 +0530

    added pagination

--- blogdev/views.py
+++ blogdev/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from . models import Post
 from django.template import loader
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
+    pages = Paginator(posts,3)
+    page_number = request.GET.get('page')
+    page_obj = pages.get_page(page_number)
     context = {
-        'posts':posts
+        'posts':page_obj
     }
     return render(request,'blogdev/post.html',context)
    
     
+
+def post(request,id):
+   post = get_object_or_404(Post,pk=id)
+   return render(request,'blogdev/detail.html',{'post':post})
--- sinhaladev/urls.py
+++ sinhaladev/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/',include('blogdev.urls')),
+    path('',include('blogdev.urls')),
+   
 ]
