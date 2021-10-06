7010f9355b93335f10a5f38191a6b2aec54df622
devimalka <developerimalka@gmail.com>
2021-10-06 15:04:12 +0530

    added templates and base template

--- blogdev/views.py
+++ blogdev/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Post
+from django.template import loader
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request,'blogdev/post.html',context)
+   
+    
