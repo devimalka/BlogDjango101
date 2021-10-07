9c9195b38064453c5f8d0d69bceceb34b6e58bac
devimalka <developerimalka@gmail.com>
2021-10-07 11:17:03 +0530

    added post category

--- blogdev/models.py
+++ blogdev/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, PROTECT
 from django_quill.fields import QuillField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
     body = QuillField()
-    created_at= models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-
-
+    category = models.ForeignKey(Category, on_delete=CASCADE)
 
     def __str__(self):
         return self.title
